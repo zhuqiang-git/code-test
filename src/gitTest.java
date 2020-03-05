7ba96dd2e42b13a9a0a6ddf30a65e7857756ecce
zhuqianggit <zhuqiangdream@126.com>
2020-03-05 21:51:32 +0800

    修改版本号为1.25

--- src/gitTest.java
+++ src/gitTest.java
@@ -2,7 +2,8 @@ public class gitTest {
 
     public static void main(String[] args) {
         //String gitVersion="1.23";
-        String gitVersion="1.24";
+        String gitVersion="1.25";
+        //String gitVersion="1.24";
         System.out.println(gitVersion);
     }
 
